dc86ff2a692dedac92be352f7dc2a620999dab76
Surbhi Airan <surbhiairan@gmail.com>
2020-05-08 14:30:24 +0530

    changes

--- server/app.ts
+++ server/app.ts
@@ -32,10 +32,10 @@ class App {
         this.app.use(express.static(path.join(__dirname, 'client/build')));
 
         if(process.env.NODE_ENV === 'production') {
-            this.app.use(express.static(path.join(__dirname, './client/build')));
+            this.app.use(express.static(path.join(__dirname, 'client/build')));
             //
             this.app.get('*', (req, res) => {
-              res.sendFile(path.join(__dirname = './client/build/index.html'));
+              res.sendFile(path.join(__dirname + './client/build/index.html'));
             })
           }
           //build mode
