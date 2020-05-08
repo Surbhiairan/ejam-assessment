68786c46b9843eef6069b8fb78d0ab661b8258d8
Surbhi Airan <surbhiairan@gmail.com>
2020-05-08 12:32:05 +0530

    client build

---
+++ client/build/precache-manifest.34d0e2a2fbfe435a414302eaf1d48d91.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edc2f09c849a6c4b01fdaadbebae0f7d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4bf0835c85111d777ad",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "a8f75d2e23c40d268a25",
+    "url": "/static/css/main.b2a3b54b.chunk.css"
+  },
+  {
+    "revision": "e4bf0835c85111d777ad",
+    "url": "/static/js/2.fc2d487c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.fc2d487c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8f75d2e23c40d268a25",
+    "url": "/static/js/main.7203b7df.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
